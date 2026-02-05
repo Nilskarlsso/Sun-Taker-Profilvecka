1135a5b3a64a9af8268e6db1cfcfdf479ca9a5dd
Carl-Samuel-Larsson <142796514+Carl-Samuel-Larsson@users.noreply.github.com>
2026-02-05 11:20:13 +0100

    BackGrounds + tiles graphic + Unlock system

--- Sun-Taker/Assets/User/Samuel/Script/Elevator.cs
+++ Sun-Taker/Assets/User/Samuel/Script/Elevator.cs
@@ -6,17 +6,29 @@ public class Elevator : MonoBehaviour
     [SerializeField] private Transform posToGo;
     [SerializeField] private ScreenFader screenFader;
 
+    [Header("Lock Settings")]
+    [SerializeField] private bool isLocked = true;
+
     private bool playerDetected;
     private GameObject playerGo;
 
     void Update()
     {
+        if (isLocked)
+            return;
+
         if (playerDetected && playerGo != null && Input.GetKeyDown(KeyCode.E))
         {
             StartCoroutine(screenFader.FadeOutIn(posToGo.position, playerGo.transform));
             playerDetected = false;
             playerGo = null;
         }
+    }
+
+    public void Unlock()
+    {
+        isLocked = false;
+        Debug.Log("Teleporter unlocked");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
--- Sun-Taker/Assets/User/Samuel/Script/ScreenFader.cs
+++ Sun-Taker/Assets/User/Samuel/Script/ScreenFader.cs
@@ -39,3 +39,4 @@ public class ScreenFader : MonoBehaviour
         SetAlpha(endAlpha);
     }
 }
+
